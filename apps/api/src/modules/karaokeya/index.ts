a83fad44dcbbeb063b86c00292eb76ceb0159263
Mauro Diego Solis <126227394+Malcomito17@users.noreply.github.com>
2025-12-04 14:05:35 -0300

    refactor(karaokeya): update module exports

--- apps/api/src/modules/karaokeya/index.ts
+++ apps/api/src/modules/karaokeya/index.ts
@@ -1,16 +1,8 @@
 /**
  * KARAOKEYA Module Exports
- * Sistema de turnos para karaoke
  */
 
-// Types
 export * from './karaokeya.types'
-
-// Routes
+export * from './karaokeya.service'
+export * from './karaokeya.controller'
 export { karaokeyaRoutes } from './karaokeya.routes'
-
-// Service (para uso interno)
-export * as karaokeyaService from './karaokeya.service'
-
-// Controller
-export * as karaokeyaController from './karaokeya.controller'
